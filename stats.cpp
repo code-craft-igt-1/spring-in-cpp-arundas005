d8bddd61fb67a16a9cceb673751be34780106e02
Arundas <51848726+arundas005@users.noreply.github.com>
2024-07-22 15:59:38 +0530

    Statistics computation integrated

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,29 @@
 #include "stats.h"
+#include <algorithm>
+#include <stdexcept>
+#include <numeric>
+#include <limits>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+namespace Statistics
+{
+	Stats ComputeStatistics(const std::vector<float>& sensorReadings) 
+	{
+		Stats computedStats;
+
+		if (sensorReadings.empty())
+		{
+			computedStats.average = std::numeric_limits<float>::quiet_NaN();
+			computedStats.min = std::numeric_limits<float>::quiet_NaN();
+			computedStats.max = std::numeric_limits<float>::quiet_NaN();
+		}
+		else
+		{
+			computedStats.min = *std::min_element(sensorReadings.begin(), sensorReadings.end());
+			computedStats.max = *std::max_element(sensorReadings.begin(), sensorReadings.end());
+			computedStats.average = std::accumulate(sensorReadings.begin(), sensorReadings.end(), 0.0f) / sensorReadings.size();
+		}
+		
+		return computedStats;
+	}
 }
